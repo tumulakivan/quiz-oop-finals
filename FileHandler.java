fea71fd7d0b442ee4f1fe9bcf9875684a697e3c9
Ivan Tumulak <ist812002@gmail.com>
2023-12-05 12:01:22 +0800

    initial commit, only test cases for user and file handler class

---
+++ FileHandler.java
@@ -0,0 +1,28 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileHandler {
+    public static void initScoreboard(String path, String text) {
+        File check_file = new File(path);
+
+        if(!check_file.exists()) {
+            try (BufferedWriter header = new BufferedWriter(new FileWriter(path))) {
+                header.write(text);
+            } catch(IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            System.out.println("File already exists: " + path);
+        }
+    }
+
+    public static void appendToFile(String path, String text) {
+        try(FileWriter user_details = new FileWriter(path, true)) {
+            user_details.write(System.lineSeparator() + text);
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ User.java
@@ -0,0 +1,21 @@
+public class User {
+    private int score;
+    private String name;
+
+    public User(String name) {
+        this.name = name;
+        this.score = 0;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getScore() {
+        return this.score;
+    }
+
+    public void setScore(int score) {
+        this.score += score;
+    }
+}
