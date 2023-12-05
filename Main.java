8c6b8bf464b9301554cd49f6d8241423647450c2
Ivan Tumulak <ist812002@gmail.com>
2023-12-05 20:00:25 +0800

    minor adjustments to main method

--- Main.java
+++ Main.java
@@ -11,12 +11,17 @@
             String user, file_name = "scoreboard.txt", file_header = "test";
             
             // FileHandler.resetBoard(file_name, file_header);
-            // FileHandler.initScoreboard(file_name, file_header);
+            FileHandler.initScoreboard(file_name, file_header);
 
             System.out.print("name: ");
             user = sc.nextLine();
             User player = new User(user);
             String toAppend = player.getName() + " - " + player.getScore();
             FileHandler.appendToFile(file_name, toAppend);
+            
         }
     }
+
+    /*
+     * Your program must be able to enter the player name, the player can then select easy, moderate or difficult mode. Create 20 questions for each mode relevant to java, the player only needs to answer 10 questions. The player has 3 lives, for each incorrect answer the player life will be deducted by 1, if life is 0, it means game over. If the player completed the calculate the total score. Apply the OOP concepts and validation (exceptions). Display the top scorers and save and retrieved it through a text file.
+     */
